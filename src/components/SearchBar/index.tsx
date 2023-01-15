6916197c06881c996c5f1fa66759b22705fac03d
Justine Hornales <jchornales.dev@gmail.com>
2023-01-15 19:30:01 +0800

    bug/GSB0001: fixed api request problem, and user data rendering

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,26 +1,37 @@
-import { ChangeEvent, useState } from 'react';
-import GetProjects from '../ProjectList';
+import { useQuery } from '@tanstack/react-query';
+import { FormEvent, useState } from 'react';
+import { fetchUser } from '../../configs/api/FetchData';
+import UsersList from '../UsersList';
 
 export default function SearchBar() {
   const [inputValue, setInputValue] = useState('');
+  const { data: users, refetch } = useQuery(['users'], fetchUser);
 
-  function handleInput(event: ChangeEvent<HTMLInputElement>) {
-    setInputValue(event.target.value);
+  function handleInput(event: FormEvent) {
+    event.preventDefault();
+    refetch;
   }
 
   return (
-    <div className="form-control flex flex-column justify-center align-center m-auto w-1/2 min-h-screen">
+    <form
+      onSubmit={handleInput}
+      className="form-control flex flex-column justify-center align-center m-auto w-1/2 min-h-screen"
+    >
       <div className="input-group">
         <input
           type="text"
           placeholder="Search…"
-          className="input w-full input-bordered border-2 focus:outline-0 focus:border-emerald"
-          onChange={handleInput}
+          className="input w-full input-bordered border-2 focus:outline-0 focus:border-emerald-500 hover:border-emerald-500"
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
         />
-        <button className="btn btn-square bg-emerald">
+        <button
+          type="submit"
+          className="btn btn-square bg-emerald-500 hover:bg-emerald-500 cursor-auto"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
+            className="w-6"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -34,8 +45,7 @@ export default function SearchBar() {
           </svg>
         </button>
       </div>
-
-      <GetProjects inputValue={inputValue} />
-    </div>
+      <UsersList inputValue={inputValue} users={users} />
+    </form>
   );
 }
